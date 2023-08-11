6be0bb78a8502687e45d034af26f3b4e2d758b5e
wesleymktd <wesleymktd@gmail.com>
2023-08-11 15:20:46 -0300

    rota POST /room implemented req7

--- src/TrybeHotel/Controllers/RoomController.cs
+++ src/TrybeHotel/Controllers/RoomController.cs
@@ -23,7 +23,7 @@ namespace TrybeHotel.Controllers
         // 7. Desenvolva o endpoint POST /room
         [HttpPost]
         public IActionResult PostRoom([FromBody] Room room){
-            throw new NotImplementedException();
+            return Created("", _repository.AddRoom(room));
         }
 
         // 8. Desenvolva o endpoint DELETE /room/:roomId
